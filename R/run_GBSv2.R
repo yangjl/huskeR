295e113fd871cec7fdcfec1d175a93e15e592e98
Jinliang Yang <jyang21@unl.edu>
2017-10-23 12:07:24 -0500

    testing gbsv2 0.1

--- R/run_GBSv2.R
+++ R/run_GBSv2.R
@@ -3,132 +3,105 @@
 #' a single slurm job.
 #' help doc: https://bitbucket.org/tasseladmin/tassel-5-source/wiki/Tassel5GBSv2Pipeline
 #'
+#' @parm outdir Out put file directory. A batch of subdir will be created under it. [chr, ="largedata/gbs"]
+#' @param shfile A shell script in "slurm-script/". [chr, "slurm-script/run_it.sh"]
+#' @param fqdir Absolute dir for the fastq files, can be nested within the dir. [chr, "my/fq/filedir"]
+#' @param keyfile The key file. The GBSv2 key file has 4 required headers: Flowcell, Lane, Barcode and FullSampleName. [chr, "data/file.txt]
 #'
-#' @param inputdf An input data.frame for this function, including all the necessary pheno, geno file pathes,
-#' and other parameters. [data.frame, see exaples]
-#' @param cv Cross-validation experiment [TRUE/FALSE, default=FALSE].
-#'           If TRUE, inputdf must contain `trainpheno` and `testpheno`; if FALSE, only need `pheno`.
-#' @param bayesType Analysis types for Bayes Methods. [chr, default=BayesC; BayesA, BayesB, BayesC, BayesCPi, RBR]
-#' @param inpdir Dir for the GS .inp file. [chr, default="largedata/"].
-#' @param cmdno Number of commands per CPU, i.e. number of rows per inputdf. [num, default=1].
-#' @param remove Whether to remove the intermediate GS files or not. [TRUE/FALSE, default is TRUE].
-#' @param shid The shell ID for this job. [chr, default is "slurm-script/run_gensel_array.sh"].
-#' @param email Your email address that farm will email to once the jobs were done/failed.
-#'           [chr, default is NULL]
-#' @param runinfo Parameters specify the array job partition information.
-#'           A vector of c(FALSE, "batch", "30G", 1, "8:00:00"): 1) run or not, default=FALSE
-#'           2) -p partition name, default=batch, 3) --ntasks=3, 4) --mem=30G, and 5) --time=8:00:00.
+#' @param db Relative file name of the db. [chr, =NULL, or "data/gbs.db"]
+#' @param mem Memory usage. [num, 50]
+#' @param kmerlen Kmer length [num, =64]
+#' @param enzyme Enzyme used for cutting. [chr, ="ApeKI"]
+#'
+#' @param seq2tag Generated tags from fastq files. [logical, =T]
+#' @param tag2fq extract tags from db for alignment to reference. [logical, =T]
+#' @param bt2 Run bt2 alignment. [logical, =T]
+#' @param snpcall Call SNP. [logical, =T]
 #'
 #' @return return a batch of shell scripts.
 #'
 #' @examples
 #'
-#' inputdf <- data.frame(pi=0.995,
-#'    geno="/Users/yangjl/Documents/GWAS2_KRN/SNP/merged/geno_chr.newbin",
-#'    trainpheno="/Users/yangjl/Documents/Heterosis_GWAS/pheno2011/reports/cd_GenSel_fullset.txt",
-#'    testpheno="/Users/yangjl/Documents/Heterosis_GWAS/pheno2011/reports/cd_GenSel_fullset.txt",
-#'    chainLength=1000, burnin=100, varGenotypic=1.4, varResidual=2,
-#'    out="test_out")
-#'
-#' run_GenSel4(inputdf, cv=FALSE, inpdir="slurm-script/", cmdno=1,
-#'             shid = "slurm-script/run_gensel_array.sh", remove=TRUE,
-#'             email=NULL, runinfo = c(FALSE, "batch", 3, "30", "8:00:00") )
 #'
 #' @export
-GBSSeqToTag <- function(){
+run_GBSv2 <- function(outdir="largedata/gbs", shfile, fqdir, kefile,
+                      db=NULL, mem, kmerlen=64, enzyme="ApeKI",
+                      seq2tag=TRUE, tag2fq=TRUE, bt2=TRUE, snpcall=TRUE){
+    #### create dir if not exist
+    dir.create("slurm-script", showWarnings = FALSE)
 
-}
-#'
-#'
-run_GenSel4 <- function(
-  inputdf, cv=FALSE, bayesType="BayesC", inpdir="largedata/", cmdno=1,
-  shid = "slurm-script/run_gensel_array.sh",
-  remove=TRUE,
-  email=NULL, runinfo = c(FALSE, "batch", "30", 3, "8:00:00")
-){
+    dir.create(paste(outdir, "tagexport", sep="/"), showWarnings = FALSE)
+    dir.create(paste(outdir, "discovery", sep="/"), showWarnings = FALSE)
+    #dir.create(paste(outdir, "db", sep="/"), showWarnings = FALSE)
 
-  #### create dir if not exist
-  dir.create("slurm-script", showWarnings = FALSE)
-  dir.create(inpdir, showWarnings = FALSE)
-
-  for(i in 1:nrow(inputdf)){
-    inpid <- paste0(inpdir, "/", inputdf$out[i], ".inp")
-    ### output the inp file:
-
-    if(cv){
-      GS_cv_inp(
-        inp= inpid, bayesType=bayesType, pi=inputdf$pi[i], geno=inputdf$geno[i],
-        trainpheno=inputdf$trainpheno[i], testpheno=inputdf$testpheno[i],
-        chainLength=inputdf$chainLength[i], burnin=inputdf$burnin[i],
-        varGenotypic=inputdf$varGenotypic[i], varResidual=inputdf$varResidual[i]
-      )
-    }else{
-      GS_regular_inp(
-        inp= inpid, bayesType=bayesType, pi=inputdf$pi[i], geno=inputdf$geno[i],
-        pheno=inputdf$trainpheno[i],
-        chainLength=inputdf$chainLength[i], burnin=inputdf$burnin[i],
-        varGenotypic=inputdf$varGenotypic[i], varResidual=inputdf$varResidual[i]
-      )
+    if(is.null(db)){
+        db = paste(outdir, "db", sep="/")
+        dir.create(db, showWarnings = FALSE)
     }
 
-  }
+    # 1. load modules
+    cat(paste("## [run_GBSv2]: Tassel5.2 GBSv2", Sys.time(), sep=" "),
+        paste0("module load java/1.8 tassel/5.2 bowtie/2.2"),
+        file=shfile, sep="\n", append=FALSE)
 
-  ### setup shell id
-  set_GS(inputdf, cmdno, inpdir, remove)
+    # 2. generate tags from fastq file
+    if(seq2tag){
+        GBSSeqToTag(shfile, mem, fqdir, db, keyfile, enzyme)
+    }
+    if(tag2fq){
+        TagExportToFastq(shfile, mem, db, tagexport, mindepth=1)
+    }
+    if(bt2){
+        run_bowtie2(shfile, cpu, bt2_idx, tagexport, sam)
+        SAMToGBSdb(shfile, sam, db)
+    }
+    if(snpcall){
+        DiscoverySNP(shfile, db, ref, mnlcov=0.05, mnmaf=0.001 )
+        SNPQuality(shfile, db, snpqc_file)
+    }
 
-  sh0 <- "module load compiler/gcc/6.1"
-  shcode <- c(sh0, paste0("sh ", inpdir, "/run_gensel_$SLURM_ARRAY_TASK_ID.sh"))
-  set_array_job(shid=shid, shcode=shcode, arrayjobs=paste("1", nrow(inputdf)/cmdno, sep="-"),
-                wd=NULL, jobid="gensel", email=email, runinfo=runinfo)
+
 }
-
 
 
 #' \code{generate tags from fastq file}
 #'
-#' @param shfile A shell script.
-#' @param mem Memory [num, =50]
-#' @param fqdir Absolute dir for the fastq files, can be nested within the dir. [chr, "my/fq/filedir"]
-#' @param db Relative file name of the db. [chr, "data/gbs.db"]
-#' @param keyfile The key file. The GBSv2 key file has 4 required headers: Flowcell, Lane, Barcode and FullSampleName. [chr, "data/file.txt]
-#'
 #'
 #' @return return a shell script.
 #'
-#' @rdname run_tassel
-GBSSeqToTag <- function(shfile, mem=50, fqdir, db, keyfile){
+#' @rdname run_GBSv2
+GBSSeqToTag <- function(shfile, mem, fqdir, db, keyfile, kmerlen, enzyme){
 
-  cat(paste("## Step1: GBSSeqToTag, Tassel5.2 GBSv2", Sys.time(), sep=" "),
+  cat(paste("## [run_GBSv2]: set GBSSeqToTag, Tassel5.2 GBSv2"),
 
-      paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -GBSSeqToTagDBPlugin -e ApeKI \\"),
+      paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -GBSSeqToTagDBPlugin -e ", enzyme, " \\"),
       paste("-i", fqdir, " \\"),
       paste("-db", db, " \\"),
       paste("-k", keyfile, " \\"),
 
       "-c 10 \\",
-      "-kmerLength 80 \\",
+      paste("-kmerLength", kmerlen, "\\",
       "-minKmerL 20 \\",
       "-mnQS 20 \\",
       "-batchSize 8 \\",
       "-endPlugin -runfork1",
 
-      file=shfile, sep="\n", append=FALSE
+      file=shfile, sep="\n", append=TRUE)
   )
 }
 
 
 #' \code{extract tags from db for alignment to reference}
 #'
-#' @param shfile A shell script.
-#' @param tagexport Fastq.gz file to export tags. [chr, ""export/tagsForAlign_F1s.fa.gz"]
+#' @param tagexport Fastq.gz file to export tags. [chr, "tagexport/tagsForAlign.fa.gz"]
 #' @param mindepth The minimum count of reads for a tag to be output. [num, =1]
 #'
 #' @return return a shell script.
 #'
-#' @rdname run_tassel
-TagExportToFastq <- function(shfile, mem=50, db, tagexport, mindepth=1){
+#' @rdname run_GBSv2
+TagExportToFastq <- function(shfile, mem, db, tagexport, mindepth=1){
 
-    cat(paste("## Step2: TagExportToFastq, Tassel5.2 GBSv2", Sys.time(), sep=" "),
+    cat(paste("## [run_GBSv2]: set TagExportToFastq, Tassel5.2 GBSv2"),
 
         paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -TagExportToFastqPlugin \\"),
         paste("-db", db, " \\"),
@@ -136,8 +109,132 @@ TagExportToFastq <- function(shfile, mem=50, db, tagexport, mindepth=1){
         paste("-c", mindepth, " \\"),
         "-endPlugin -runfork1",
 
-        file=shfile, sep="\n", append=FALSE
-    )
+        file=shfile, sep="\n", append=TRUE)
 }
+
+#' \code{map tags to reference use bt2}
+#' #--very-sensitive-local -D 20 -R 3 -N 0 -L 20 -i S,1,0.50
+#' pigz -k -d -p 50 Zea_mays.AGPv4.dna.toplevel.fa.gz
+#' bowtie2-build Zea_mays.AGPv4.dna.toplevel.fa AGPv4_bt2_index --threads 50
+#'
+#' @param cpu Number of CPUs to use. [num, 50]
+#' @param bt2_idx The idx file for bowtie2. [chr, "/lustre/work/jyanglab/jyang21/dbcenter/AGP/AGPv4/AGPv4_bt2_index/AGPv4_bt2_index"]
+#' @param sam The output sam file. [chr, "tagexport/tagsForAlignFullvs_F1s.sam"]
+#'
+#' @return return a shell script.
+#'
+#' @rdname run_GBSv2
+run_bowtie2 <- function(shfile, cpu, bt2_idx, tagexport, sam){
+
+    cat(paste("## [run_GBSv2]: set bowtie2, Tassel5.2 GBSv2"),
+
+        paste0("bowtie2 -p ", cpu, " \\"),
+        "--very-sensitive-local \\",
+        paste("-x", bt2_idx, " \\"),
+        paste("-U", tagexport, " \\"),
+        paste("-S", sam, " \\"),
+        file=shfile, sep="\n", append=TRUE)
+}
+
+
+#' \code{store tag physical map position in database}
+#'
+#' @return return a shell script.
+#'
+#' @rdname run_GBSv2
+SAMToGBSdb <- function(shfile, sam, db){
+
+    cat(paste("## [run_GBSv2]: set SAMToGBSdb, Tassel5.2 GBSv2"),
+
+        paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -SAMToGBSdbPlugin \\"),
+        paste("-i", sam, " \\"),
+        paste("-db", db, " \\"),
+
+        paste("-aProp 0 \\"),
+        paste("-aLen 0 \\"),
+        "-endPlugin -runfork1",
+        file=shfile, sep="\n", append=TRUE)
+}
+
+
+#' \code{run SNP discovery plugin}
+#'
+#' @param ref Fastq file of the ref. [chr, "/lustre/work/jyanglab/jyang21/dbcenter/AGP/AGPv4/AGPv4_bt2_index/AGPv4_bt2.fa"]
+#' @param mnlcov mnLCov. [num, =0.05]
+#' @param mnmaf Min MAF. [num, =0.001]
+#' @param deleteolddata Delete Old Data. [chr, ="true"]
+#'
+#' @return return a shell script.
+#'
+#' @rdname run_GBSv2
+DiscoverySNP <- function(shfile, db, ref, mnlcov=0.05, mnmaf=0.001 ){
+
+    cat(paste("## [run_GBSv2]: set DiscoverySNPCaller, Tassel5.2 GBSv2"),
+
+        paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -DiscoverySNPCallerPluginV2 \\"),
+        paste("-db", db, " \\"),
+        paste("-ref", ref, " \\"),
+
+        paste("-mnLCov", mnlcov, " \\"),
+        paste("-mnMAF", mnmaf, " \\"),
+        paste("-deleteOldData", deleteolddata, " \\"),
+        "-endPlugin -runfork1",
+        file=shfile, sep="\n", append=TRUE)
+}
+
+
+#' \code{check SNP quality parameters}
+#'
+#' @param snpqc_file SNP qc file. [chr, "largedata/gbs/outputStats.txt"]
+#' @param mnlcov mnLCov. [num, =0.05]
+#' @param mnmaf Min MAF. [num, =0.001]
+#' @param deleteolddata Delete Old Data. [chr, ="true"]
+#'
+#' @return return a shell script.
+#'
+#' @rdname run_GBSv2
+SNPQuality <- function(shfile, db, snpqc_file){
+
+    cat(paste("## [run_GBSv2]: set SNPQualityProfilerPlugin, Tassel5.2 GBSv2"),
+
+        paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -SNPQualityProfilerPlugin \\"),
+        paste("-db", db, " \\"),
+        paste("-statFile", snpqc_file, " \\"),
+
+        "-endPlugin -runfork1",
+        file=shfile, sep="\n", append=TRUE)
+}
+
+
+
+#' \code{run production pipeline}
+#'
+#' @param h5 Output hdf5 format. [chr, "gbs/discovery/output.h5"]
+#' @return return a shell script.
+#'
+#' @rdname run_GBSv2
+ProductionSNPCaller <- function(shfile, db, enzyme, fqdir, keyfile, kmerlen, h5){
+
+    cat(paste("## [run_GBSv2]: set ProductionSNPCallerPluginV2, Tassel5.2 GBSv2"),
+
+        paste0("run_pipeline.pl -Xmx", mem, "g -fork1 -ProductionSNPCallerPluginV2 \\"),
+        paste("-db", db, " \\"),
+        paste("-e", enzyme, " \\"),
+        paste("-i", fqdir, " \\"),
+        paste("-k", keyfile, " \\"),
+        paste("-kmerLength", kmerlen, "\\"),
+        paste("-o", h5, "\\"),
+
+        "-endPlugin -runfork1",
+        file=shfile, sep="\n", append=TRUE)
+}
+
+
+
+
+
+
+
+
 
 
