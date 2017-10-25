699ccf132f9686a7193068e6586c5664a72bc415
Jinliang Yang <yangjl0930@gmail.com>
2017-10-25 09:19:09 -0500

    fq-dump tested

--- R/run_fq_dump.R
+++ R/run_fq_dump.R
@@ -7,7 +7,7 @@
 #'
 #' @param inputdf sra files in df as input. no need path info. [df, df["sra"]]
 #' @param dumppath The absolute path of the SRA files to dump. [chr, "largedata/dump/"]
-#' @param slurmsh File name of the output shell command. [chr, "slurm-script/dump.sh"]
+#' @param slurmsh File name of the output shell command. [chr, ="slurm-script/run_dump_"]
 #' @param rmsra Remove the original SRA file after dumpping. [logical, =TRUE]
 #' @param email Your email address that farm will email to once the job was done/failed. [chr, "y@gmail.com"]
 #' @param gzip GZIP the fastq files. [logical, =FALSE]
@@ -25,14 +25,14 @@
 #' run_fq_dump2(filepath="test", rmsra=TRUE, gzip=TRUE, email=NULL, run=c(TRUE, "bigmemh", "8196", "1"))
 #'
 #' @export
-run_fq_dump <- function(inputdf, rmsra=FALSE, gzip=TRUE, email=NULL,
-                        runinfo=c(FALSE, "bigmemh", 5, "5G", "16:00:00")){
+run_fq_dump <- function(inputdf, dumppath, rmsra=FALSE, gzip=FALSE, email=NULL,
+                        slurmsh="slurm-script/run_dump_", runinfo=c(FALSE, "bigmemh", 5, "5G", "16:00:00")){
 
   files <- inputdf$sra
   dir.create("slurm-script", showWarnings = FALSE)
   for(i in 1:length(files)){
 
-    shid <- paste0("slurm-script/run_dump_", i, ".sh")
+    shid <- paste0(slurmsh, i, ".sh")
     cmd1 <- paste0("cd ", dumppath)
     cmd2 <- paste0("fastq-dump --split-spot --split-3 -A ", files[i])
     cmd <- c(cmd1, cmd2)
@@ -48,8 +48,9 @@ run_fq_dump <- function(inputdf, rmsra=FALSE, gzip=TRUE, email=NULL,
     cat(cmd, file=shid, sep="\n", append=FALSE)
   }
 
-  shcode <- paste("sh slurm-script/run_dump_$SLURM_ARRAY_TASK_ID.sh", sep="\n")
-  set_array_job(shid="slurm-script/run_dump_array.sh",
+  shcode <- paste0("module load SRAtoolkit/2.8; sh ",
+                   slurmsh, "$SLURM_ARRAY_TASK_ID.sh", sep="\n")
+  set_array_job(shid=paste0(slurmsh, "array.sh"),
                 shcode=shcode, arrayjobs=paste("1", length(files), sep="-"),
                 wd=NULL, jobid="aspera", email=email, runinfo=runinfo)
 }
