0db9f925091f542e06e34010ddaea56a13198e59
Jinliang Yang <yangjl0930@gmail.com>
2018-04-21 16:30:55 -0500

    update plot_mht

--- R/plot_mht.R
+++ R/plot_mht.R
@@ -1,16 +1,17 @@
 #' \code{manhattan plot}
 #'
 #'
-#' @param inputdf An input data.frame to plot. plot_mht: [df, =col2plot, "chr", "pos"]; plot_jmht: [,cex].
+#' @param inputdf An input data.frame to plot. plot_mht: [df, =col2plot, "chr", "pos"]
 #' @param col2plot The column name to plot. [chr, ="ModelFreq"]
 #' @param cl_file The full path of the chromosome length file. [chr, ="~/bin/zmSNPtools/Rcodes/chr_length_B73v3.csv"]
 #' @param CAP Cap between chrs. [num, =5e+06]
+#' @param jmph Plot joint mht. [log, =FALSE], if =TURE, must have cex weight column (cw) in inputdf: [,cw].
 #'
 #' @export
 plot_mht <- function(inputdf, col2plot="ModelFreq",
                      cl_file = "~/bin/zmSNPtools/Rcodes/chr_length_B73v3.csv",
                      cex=.9, pch=16, col=rep(c("slateblue", "cyan4"), 5),
-                     GAP=5e+06, yaxis=NULL,
+                     GAP=5e+06, yaxis=NULL, jmph=FALSE,
                       ... ){
 
     res <- inputdf
@@ -36,45 +37,17 @@ plot_mht <- function(inputdf, col2plot="ModelFreq",
   abline(v=chrtick$chrlines, col="grey")
 
   for(i in 1:10){
-    points(x=subset(res, chr==i)$newpos, y=res[res$chr==i, col2plot],
-           pch = pch, col=col[i], cex=cex);
+      if(jmph){
+          points(x=subset(res, chr==i)$newpos, y=res[res$chr==i, col2plot],
+                 pch = pch, col=col[i], cex=res[res$chr==i, ]$cw*cex);
+      }else{
+          points(x=subset(res, chr==i)$newpos, y=res[res$chr==i, col2plot],
+                 pch = pch, col=col[i], cex=cex);
+      }
+
   }
 }
 
-#' @rdname plot_mht
-plot_jmht <- function(inputdf, col2plot="ModelFreq",
-                     cl_file = "~/bin/zmSNPtools/Rcodes/chr_length_B73v3.csv",
-                     cex=.9, pch=16, col=rep(c("slateblue", "cyan4"), 5),
-                     GAP=5e+06, yaxis=NULL,
-                     ... ){
-
-    res <- inputdf
-    cl <- read.csv(cl_file)
-
-    res <- newpos(d=res, GAP, cl)
-    chrtick <- chrline_tick(GAP, cl)
-
-
-    #### setup the cavon
-    if(is.null(yaxis)){
-        plot(x=-1000, y=-1000,  type="p", xaxt="n", xlab="",
-             xlim=c(0, max(chrtick$chrlines)), ylim=c(0, max(res[, col2plot], na.rm=TRUE)*1.3 ),
-             ...)
-    }else{
-        plot(x=-1000, y=-1000,  type="p", xaxt="n", yaxt="n", xlab="",
-             xlim=c(0, max(chrtick$chrlines)),
-             ...)
-        axis(side=2, at=NULL, labels=yaxis)
-    }
-    axis(side=1, at=chrtick$ticks, labels=c("chr1", "chr2", "chr3", "chr4", "chr5",
-                                            "chr6", "chr7", "chr8", "chr9", "chr10"))
-    abline(v=chrtick$chrlines, col="grey")
-
-    for(i in 1:10){
-        points(x=subset(res, chr==i)$newpos, y=res[res$chr==i, col2plot],
-               pch = pch, col=col[i], cex=res[res$chr==i, ]$cex);
-    }
-}
 
 #' @rdname plot_mht
 newpos <- function (d, GAP, cl)
