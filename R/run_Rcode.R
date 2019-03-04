a13af739d5b31cdcccba5e2ce7cf2388ce5adeb0
Jinliang Yang <yangjl0930@gmail.com>
2019-03-04 11:59:52 -0600

    minor

--- R/run_Rcode.R
+++ R/run_Rcode.R
@@ -38,7 +38,7 @@ run_Rcode <- function(
     tot <- ceiling(nrow(inputdf)/cmdno)
     for(j in 1:tot){
 
-        shid <- paste0(outdir, base_shid, "_", j, ".sh")
+        shid <- paste0(outdir, "/", base_shid, "_", j, ".sh")
 
         ##chr:start-end
         #sh1 <- paste("cd", outdir)
@@ -50,7 +50,7 @@ run_Rcode <- function(
     }
 
     shcode <- paste0("module load R/", rversion, "; sh ", outdir, "/run_rcode_$SLURM_ARRAY_TASK_ID.sh")
-    arrayshid <- paste0(outdir, base_shid, "_array.sh")
+    arrayshid <- paste0(outdir, "/", base_shid, "_array.sh")
     set_array_job(shid=arrayshid, shcode=shcode, arrayjobs=paste("1", tot, sep="-"),
                   wd=NULL, jobid=base_shid, email=email, runinfo=runinfo)
 }
